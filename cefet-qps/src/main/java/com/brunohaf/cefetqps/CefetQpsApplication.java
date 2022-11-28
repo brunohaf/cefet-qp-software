c4b839d05e1fd0315bca593eb5c5e520bbac6033
Bruno Fonseca <bruno3145@gmail.com>
2022-11-28 18:19:33 -0300

    first commit

---
+++ cefet-qps/src/main/java/com/brunohaf/cefetqps/CefetQpsApplication.java
@@ -0,0 +1,13 @@
+package com.brunohaf.cefetqps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CefetQpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CefetQpsApplication.class, args);
+	}
+
+}
---
+++ cefet-qps/src/test/java/com/brunohaf/cefetqps/CefetQpsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunohaf.cefetqps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CefetQpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
